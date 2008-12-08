07bc40182a74bca3fa033504b72bae442e025dea
Roman Zybin <phoenis@spidey.linux-foundation.org>
2008-12-08 06:39:11 -0800

    bug 2442, 2458 fixed

--- src/testdata/install_testdata.sh
+++ src/testdata/install_testdata.sh
@@ -2,19 +2,26 @@ DIR=${0%/*}
 cd $DIR
 
 echo -n "Installing test data for locale.nlcat ...      "
-pushd locale.nlcat>/dev/null 2>&1
+cd locale.nlcat
 gencat -o catfirst.cat catfirst.msg
 gencat -o catsecond.cat catsecond.msg
 gencat -o catthird.cat catthird.msg
-popd>/dev/null 2>&1
+cd ..
 echo 'Done'
 
 echo -n "Installing test data for locale.textdomain ... "
-pushd locale.textdomain>/dev/null 2>&1
-find . -name '*.po'>makefile 2>/dev/null
-sed -r 's/(.+).po/msgfmt \1.po -o \1.mo/' makefile -i
-chmod +x makefile
-./makefile
-rm -f makefile
-popd>/dev/null 2>&1
+cd locale.textdomain
+find . -name '*.po'>genmo 2>/dev/null
+sed -r 's/(.+).po/msgfmt \1.po -o \1.mo/' genmo -i
+chmod +x genmo
+./genmo
+rm -f genmo
+cd ..
 echo 'Done'
+
+echo -n "Installing Olver-locale for locale.locale ... "
+cd locale.locale
+./install_locale.sh
+cd ..
+echo 'Done'
+
--- src/testdata/uninstall_testdata.sh
+++ src/testdata/uninstall_testdata.sh
@@ -2,15 +2,15 @@ DIR=${0%/*}
 cd $DIR
 
 echo -n "Uninstalling test data for locale.nlcat ...      "
-pushd locale.nlcat>/dev/null 2>&1
+cd locale.nlcat
 rm -f catfirst.cat
 rm -f catsecond.cat
 rm -f catthird.cat
-popd>/dev/null 2>&1
+cd ..
 echo 'Done'
 
 echo -n "Uninstalling test data for locale.textdomain ... "
-pushd locale.textdomain>/dev/null 2>&1
+cd locale.textdomain
 find . -name '*.mo' -exec rm -f '{}' \; 2>/dev/null
-popd>/dev/null 2>&1
+cd ..
 echo 'Done'
